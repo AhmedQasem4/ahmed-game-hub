2332d7e3459c9700f9b8f48c58fdd781745991fd
ahmed Qasem <abok2ssem14@gmail.com>
2023-09-12 07:42:10 +0300

    build useGames hook to fetch games.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
+const apiClient = axios.create({
     baseURL: 'https://api.rawg.io/api',
     params:{
         key: 'c302c1502fa14e229892f54cfcef41ea',
@@ -8,4 +8,4 @@ const axiosInstance = axios.create({
 });
 
 
-export default axiosInstance;
+export default apiClient;
