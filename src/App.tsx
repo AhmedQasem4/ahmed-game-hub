bec61e0158ff822422ea915884596745c68a018d
ahmed Qasem <abok2ssem14@gmail.com>
2023-09-13 10:29:39 +0300

    Building Sorting Logic

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,11 @@ import PlatformSelector from './components/PlatformSelector';
 import { Platform } from './hooks/usePlatforms';
 import SortSelector from './components/SortSelector';
 
+
 export interface GameQuery{
   genre: Genres | null;
   platform : Platform | null;
+  sortOrder: string;
 }
 
 function App() {
@@ -37,7 +39,7 @@ function App() {
     <GridItem  area={"main"}>
       <HStack spacing={5} marginLeft={5} marginBottom={2}>
         <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform)=> setGameQuery({...gameQuery , platform})} />
-        <SortSelector />
+        <SortSelector sortOrde={gameQuery.sortOrder} onSelectSortOrder={(sortOrder)=> setGameQuery({...gameQuery , sortOrder})} />
       </HStack>
       <GameGrid gameQuery={gameQuery}/>
     </GridItem>
--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,21 +1,41 @@
-import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
-import React from 'react'
-import { BsChevronDown } from 'react-icons/bs'
+import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import React from "react";
+import { BsChevronDown } from "react-icons/bs";
+import { GameQuery } from "../App";
 
-const SortSelector = () => {
-  return (
-    <Menu>
-        <MenuButton as={Button} rightIcon={<BsChevronDown />}>order by: Relevance</MenuButton>
-        <MenuList>
-            <MenuItem>Relevance</MenuItem>
-            <MenuItem>Data added</MenuItem>
-            <MenuItem>Name</MenuItem>
-            <MenuItem>Relase date</MenuItem>
-            <MenuItem>popularity</MenuItem>
-            <MenuItem>Average rating</MenuItem>
-        </MenuList>
-    </Menu>
-  )
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrde: string;
 }
 
-export default SortSelector
+const SortSelector = ({ onSelectSortOrder, sortOrde }: Props) => {
+  const sortOrder = [
+    { value: "", label: "Relevence" },
+    { value: "-added", label: "Data added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Relase date" },
+    { value: "-metacritic", label: "popularity" },
+    { value: "-rating", label: "Average rating" },
+  ];
+  const currentSortOrder = sortOrder.find(item => item.value === sortOrde);
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Order by: {currentSortOrder?.label || 'Relevance'}
+      </MenuButton>
+      <MenuList>
+        {sortOrder.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            value={order.value}
+            key={order.value}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
