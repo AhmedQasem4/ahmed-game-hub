d29a8e336539a046a5c37b1633f24871bb5cab18
ahmed Qasem <abok2ssem14@gmail.com>
2023-09-13 11:52:20 +0300

    Building Emoji

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,6 +5,7 @@ import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCropedImageUrl from "./../services/image-url";
 import GameCardContainer from "./GameCardContainer";
+import Emoji from "./Emoji";
 
 interface Props {
   game: Game;
@@ -24,6 +25,7 @@ const GameCard = ({ game }: Props) => {
           <CriticScore score={game.metacritic} />
         </HStack>
         <Heading marginTop={3} fontSize="2xl">{game.name}</Heading>
+        <Emoji rating={game.rating_top} />
       </CardBody>
     </Card>
   );
