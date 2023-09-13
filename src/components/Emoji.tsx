8126b64299b64d0054f82bba9ec35406afee8f2e
ahmed Qasem <abok2ssem14@gmail.com>
2023-09-13 11:53:11 +0300

    adding margin top to emoji

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -12,10 +12,9 @@ const Emoji = ({rating}: Props) => {
         3: {src: meh , alt: 'meh', boxSize: '25px'},
         4: {src: thumbsUp , alt: 'recommended', boxSize: '25px'},
         5: {src: bullsEye , alt: 'exceptional', boxSize: '35px'},
-
     }
   return (
-    <Image {...emojiMap[rating]} />
+    <Image marginTop={3} {...emojiMap[rating]} />
   )
 }
 
